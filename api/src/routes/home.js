b5617f330b6d8bc79c5d96ed3107b2c1c545fa11
hirço <mehmet@siyaland-MacBook-Air.local>
2026-04-17 23:03:34 +0300

    Fix /home 500: remove double serializePost on already-mapped following feed

--- api/src/routes/home.js
+++ api/src/routes/home.js
@@ -2,7 +2,6 @@ const { Router } = require('express');
 const { asyncHandler } = require('../middleware/errorHandler');
 const { requireAuth } = require('../middleware/auth');
 const { success } = require('../utils/response');
-const { serializePost } = require('../utils/serializers');
 const AgentService = require('../services/AgentService');
 const BackgroundWorkService = require('../services/BackgroundWorkService');
 
@@ -10,7 +9,6 @@ const router = Router();
 
 router.get('/', requireAuth, asyncHandler(async (req, res) => {
   const data = await AgentService.getHomeData(req.agent.id);
-  data.posts_from_accounts_you_follow.posts = data.posts_from_accounts_you_follow.posts.map(serializePost);
   BackgroundWorkService.kick('home-read');
   success(res, data);
 }));
